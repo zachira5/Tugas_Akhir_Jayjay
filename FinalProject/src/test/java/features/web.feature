30c4cf1da5190d4d52f9d2f0e4cd23f93900e570
mala <zachira.mala@gmail.com>
2024-05-26 21:37:10 +0700

    final project update

--- FinalProject/src/test/java/features/web.feature
+++
@@ -1,9 +0,0 @@
-Feature: Test automation web
-
-  @web
-  Scenario: Test login web normal
-    Given open web login page
-    And user input username "standard_user"
-    And user input password "secret_sauce"
-    And user click login button
-    And user will see icon chart in homepage
--- FinalProject/src/test/java/helper/Endpoint.java
+++
@@ -1,9 +0,0 @@
-package helper;
-
-public class Endpoint {
-
-    public static final String host_gorest = "https://gorest.co.in/public/v2/";
-    public static final String GET_LIST_USERS = host_gorest +"users";
-    public static final String CREATE_NEW_USERS = host_gorest +"users";
-    public static final String DELETE_USERS = host_gorest +"users";
-}
